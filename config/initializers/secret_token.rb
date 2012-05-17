794c657859725922d17fea00157a723d95eb481f
Johnno Loggie <johnno.loggie@wildfireapp.com>
2012-05-17 11:11:23 +0100

    Empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orson::Application.config.secret_token = '0347076c32991b41c7c5e3f4c8a93a203989bb327bad7b82c775b6ea3da92bcd96d40e83af34e6041afa471cae219643ee769acd70a729d0af8d109b16164de1'
