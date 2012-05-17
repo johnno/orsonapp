5eeef961ba85bda19c73fdde68e8b8d9b3ef1061
Johnno Loggie <johnno.loggie@wildfireapp.com>
2012-05-17 15:57:15 +0100

    Loading rally creds from environment

--- config/initializers/rally.rb
+++ config/initializers/rally.rb
@@ -1,4 +1,9 @@
-config_file = File.join(Rails.root,'config','rally.yml')
-data = YAML.load(ERB.new(File.read(config_file)).result)[Rails.env]
-RALLY_USER = data['username']
-RALLY_PASS = data['password']
+if Rails.env == 'production'
+  RALLY_USER = ENV['RALLY_USER']
+  RALLY_PASS = ENV['RALLY_PASS']
+else
+  config_file = File.join(Rails.root,'config','rally.yml')
+  data = YAML.load(ERB.new(File.read(config_file)).result)[Rails.env]
+  RALLY_USER = data['username']
+  RALLY_PASS = data['password']
+end
