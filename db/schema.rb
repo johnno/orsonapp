66d9a002cc4bdd9456eab973119961acd12806f7
Thomas Pelletier <pelletier.thomas@gmail.com>
2012-05-18 14:31:52 +0100

    Create task base.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120518112439) do
+ActiveRecord::Schema.define(:version => 20120518132911) do
+
+  create_table "tasks", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "state",      :default => "Defined"
+    t.datetime "created_at",                        :null => false
+    t.datetime "updated_at",                        :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
